eb829b78ffadbe16df49227073ecba93c3e19177
Alejandro Celaya <alejandro@alejandrocelaya.com>
2019-07-19 19:54:39 +0200

    Moved all event-dispatching stuff to its own module

---
+++ src/Listener/AsyncEventListener.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Shlinkio\Shlink\EventDispatcher\Listener;
+
+use Shlinkio\Shlink\EventDispatcher\Async\Task;
+use Swoole\Http\Server as HttpServer;
+
+class AsyncEventListener
+{
+    /** @var string */
+    private $regularListenerName;
+    /** @var HttpServer */
+    private $server;
+
+    public function __construct(HttpServer $server, string $regularListenerName)
+    {
+        $this->regularListenerName = $regularListenerName;
+        $this->server = $server;
+    }
+
+    public function __invoke(object $event): void
+    {
+        $this->server->task(new Task($this->regularListenerName, $event));
+    }
+}
