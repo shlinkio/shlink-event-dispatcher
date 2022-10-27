19e9ad3b5d007c7f8db14551f9b37597d4a3abbc
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-10-27 20:16:16 +0200

    Updated TaskFinishListenerTest to use PHPUnit mocks intead of prophecy

--- test/Swoole/TaskFinishListenerTest.php
+++ test/Swoole/TaskFinishListenerTest.php
@@ -5,34 +5,29 @@ declare(strict_types=1);
 namespace ShlinkioTest\Shlink\EventDispatcher\Swoole;
 
 use Mezzio\Swoole\Event\TaskFinishEvent;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\PhpUnit\ProphecyTrait;
-use Prophecy\Prophecy\ObjectProphecy;
 use Psr\Log\LoggerInterface;
 use Shlinkio\Shlink\EventDispatcher\Swoole\TaskFinishListener;
 
 class TaskFinishListenerTest extends TestCase
 {
-    use ProphecyTrait;
-
     private TaskFinishListener $listener;
-    private ObjectProphecy $logger;
+    private MockObject & LoggerInterface $logger;
 
     public function setUp(): void
     {
-        $this->logger = $this->prophesize(LoggerInterface::class);
-        $this->listener = new TaskFinishListener($this->logger->reveal());
+        $this->logger = $this->createMock(LoggerInterface::class);
+        $this->listener = new TaskFinishListener($this->logger);
     }
 
     /** @test */
     public function loggerIsCalledWhenListenerIsInvoked(): void
     {
-        $event = $this->prophesize(TaskFinishEvent::class);
-        $getTaskId = $event->getTaskId()->willReturn(123);
+        $event = $this->createMock(TaskFinishEvent::class);
+        $event->expects($this->once())->method('getTaskId')->willReturn(123);
+        $this->logger->expects($this->once())->method('notice')->with('Task {taskId} has finished', ['taskId' => 123]);
 
-        ($this->listener)($event->reveal());
-
-        $getTaskId->shouldHaveBeenCalledOnce();
-        $this->logger->notice('Task {taskId} has finished', ['taskId' => 123])->shouldHaveBeenCalledOnce();
+        ($this->listener)($event);
     }
 }
