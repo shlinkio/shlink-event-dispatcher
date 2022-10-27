2082f602eabdf5ee817ae872e47f8188439069cf
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-10-27 20:03:01 +0200

    Updated RoadRunnerTaskListenerTest to use PHPUnit mocks intead of prophecy

--- test/RoadRunner/RoadRunnerTaskListenerTest.php
+++ test/RoadRunner/RoadRunnerTaskListenerTest.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 namespace ShlinkioTest\Shlink\EventDispatcher\RoadRunner;
 
 use JsonSerializable;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Prophecy\PhpUnit\ProphecyTrait;
-use Prophecy\Prophecy\ObjectProphecy;
 use Shlinkio\Shlink\EventDispatcher\RoadRunner\RoadRunnerTaskListener;
 use Spiral\RoadRunner\Jobs\JobsInterface;
 use Spiral\RoadRunner\Jobs\QueueInterface;
@@ -17,16 +16,14 @@ use stdClass;
 
 class RoadRunnerTaskListenerTest extends TestCase
 {
-    use ProphecyTrait;
-
     private RoadRunnerTaskListener $listener;
-    private ObjectProphecy $jobs;
+    private MockObject & JobsInterface $jobs;
     private string $listenerServiceName = 'service';
 
     public function setUp(): void
     {
-        $this->jobs = $this->prophesize(JobsInterface::class);
-        $this->listener = new RoadRunnerTaskListener($this->jobs->reveal(), $this->listenerServiceName);
+        $this->jobs = $this->createMock(JobsInterface::class);
+        $this->listener = new RoadRunnerTaskListener($this->jobs, $this->listenerServiceName);
     }
 
     /**
@@ -35,23 +32,19 @@ class RoadRunnerTaskListenerTest extends TestCase
      */
     public function expectedTaskIsDispatchedBasedOnProvidedEvent(object $event, array $expectedPayload): void
     {
-        $queue = $this->prophesize(QueueInterface::class);
-        $task = $this->prophesize(PreparedTaskInterface::class);
+        $queue = $this->createMock(QueueInterface::class);
+        $task = $this->createMock(PreparedTaskInterface::class);
 
-        $connect = $this->jobs->connect('shlink')->willReturn($queue->reveal());
-        $create = $queue->create($event::class, [
+        $this->jobs->expects($this->once())->method('connect')->with('shlink')->willReturn($queue);
+        $queue->expects($this->once())->method('create')->with($event::class, [
             'listenerServiceName' => $this->listenerServiceName,
             'eventPayload' => $expectedPayload,
-        ])->willReturn($task->reveal());
-        $dispatch = $queue->dispatch($task->reveal())->willReturn(
-            $this->prophesize(QueuedTaskInterface::class)->reveal(),
+        ])->willReturn($task);
+        $queue->expects($this->once())->method('dispatch')->with($task)->willReturn(
+            $this->createMock(QueuedTaskInterface::class),
         );
 
         ($this->listener)($event);
-
-        $connect->shouldHaveBeenCalledOnce();
-        $create->shouldHaveBeenCalledOnce();
-        $dispatch->shouldHaveBeenCalledOnce();
     }
 
     public function provideEvents(): iterable
